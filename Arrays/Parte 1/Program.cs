0a735ce8c907f05021f08a775395f88c6feea987
Luca Simaque <luccasimaque@yahoo.com.br>
2021-05-10 00:06:53 -0300

    Inserindo pasta Arrays e Parte 1

---
+++ Arrays/Parte 1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aulas_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            Um array possue só dados do mesmo tipo, é ordenado e acessado por posições e é alocado de uma unica vez
+
+            Tamanho fixo e apresenta uma dificuldade para inserir e deletar dados
+            */
+
+            System.Console.Write("Quantidade de itens do Array: ");
+            int n = int.Parse(Console.ReadLine());
+
+            // iniciando vetor
+
+            int[] vetor = new int[n];
+
+            // inserindo dados
+
+            for (int i = 0; i < n; i++)
+            {
+                System.Console.Write("Valor a ser inserido na posição " + i + ": ");
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+
+            double soma = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                soma += vetor[i];
+            }
+
+            double media = soma / n;
+
+            System.Console.WriteLine("Media: " + media);
+
+        }
+    }
+
+}
