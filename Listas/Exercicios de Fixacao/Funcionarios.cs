e096d66d8ffa376b67bd01fa55bf1fca86a10381
Luca Simaque <luccasimaque@yahoo.com.br>
2021-05-12 23:39:01 -0300

    Finalização do exercicio de fixação

--- Listas/Exercicios de Fixacao/Funcionarios.cs
+++ Listas/Exercicios de Fixacao/Funcionarios.cs
@@ -4,9 +4,20 @@ namespace Exercicios_de_Fixacao
     {
         public int ID { get; set; }
         public string Nome { get; set; }
-        public double Salario { get; }
+        public double Salario { get; private set; }
 
+        public Funcionarios(int id, string nome, double salario)
+        {
+            ID = id;
+            Nome = nome;
+            Salario = salario;
 
+        }
+
+        public void AumentarSalario(int porcentagem)
+        {
+            Salario += Salario * porcentagem / 100;
+        }
 
     }
 }
--- Listas/Exercicios de Fixacao/Program.cs
+++ Listas/Exercicios de Fixacao/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Collections.Generic;
 
 namespace Exercicios_de_Fixacao
 {
@@ -7,8 +8,10 @@ namespace Exercicios_de_Fixacao
     {
         static void Main(string[] args)
         {
-            Console.Write("Deseja registrar quantos funcionarios?");
+            Console.Write("Deseja registrar quantos funcionarios? ");
             int N = int.Parse(Console.ReadLine());
+
+            List<Funcionarios> funcionarios = new List<Funcionarios>();
 
             for (int i = 1; i <= N; i++)
             {
@@ -23,8 +26,36 @@ namespace Exercicios_de_Fixacao
                 System.Console.Write("Salário: ");
                 double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+                System.Console.WriteLine();
 
+                funcionarios.Add(new Funcionarios(id, nome, salario));
+            }
 
+            System.Console.Write("Informe o ID do funcionario que deseja aumentar o salario: ");
+            int posFuncionario = int.Parse(Console.ReadLine());
+
+            bool existeID = funcionarios.Exists(x => x.ID == posFuncionario);
+
+            if (existeID)
+            {
+                System.Console.Write("Informe a porcentagem de aumento: ");
+                int quantidade = int.Parse(Console.ReadLine());
+
+                posFuncionario = funcionarios.FindIndex(x => x.ID == posFuncionario);
+                funcionarios[posFuncionario].AumentarSalario(quantidade);
+            }
+            else
+            {
+                System.Console.WriteLine("Esse ID não existe!");
+            }
+
+            System.Console.WriteLine();
+
+            System.Console.WriteLine("Lista atualizada dos funcionarios:");
+
+            foreach (var item in funcionarios)
+            {
+                System.Console.WriteLine(item.ID + ", " + item.Nome + ", " + item.Salario);
             }
         }
     }
